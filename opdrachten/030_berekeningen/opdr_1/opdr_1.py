b1ff9224ecd02bbede6205e19dccff498d0a85d9
97092925 <97092925@st.deltion.nl>
2025-03-26 14:54:30 +0100

    klaar

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,13 @@
 # Groep:
 
 # Hier komt je code...
+alice = 25
+bob = 30
+charlie = 27
+
+totale_leeftijd = alice + bob + charlie
+gemiddelde_leeftijd = totale_leeftijd / 3
+
+print("Dit weten we over Alice, Bob en Charlie:")
+print(f"De totale waarde van alle leeftijden bij elkaar opgeteld = {totale_leeftijd}")
+print(f"De gemiddelde leeftijd van deze boys is: {gemiddelde_leeftijd:.0f}")
