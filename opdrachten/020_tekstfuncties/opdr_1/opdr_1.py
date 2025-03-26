2a4c4c1b9cc279dee006043fa4da2551793951e5
97092925 <97092925@st.deltion.nl>
2025-03-26 14:33:48 +0100

    klaar

--- opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
+++ opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
@@ -16,4 +16,9 @@ voornaam = "albert"
 achternaam = "einstein"
 
 
-print()
+print("O mamma mia, wat maak je toch heerlijke pizza's")
+
+voornaam = "Albert"
+achternaam = "Einstein"
+
+print(f"{voornaam} {achternaam} zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'")
