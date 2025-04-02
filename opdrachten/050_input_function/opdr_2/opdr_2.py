cd6dd94155a858176edb8b06008c7d1c10248599
97092925 <97092925@st.deltion.nl>
2025-04-02 14:46:27 +0200

    klaar

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -1,7 +1,12 @@
-# Opdracht 2 berekeningen
-# Naam student:
-# Groep:
+# Stap 1: Maak de gastenlijst
+lijst = ["Jij", "Paul", "Kees", "Marie", "Hilda"]
+print(lijst)
 
-# Hier komt je code...
+# Stap 2: Verwijder Marie uit de lijst
+lijst.remove("Marie")
+print(lijst)
 
-gasten = ["Jij", ... ]
+# Stap 3: Voeg George toe naast Kees
+index_kees = lijst.index("Kees")  # Vind de index van Kees
+lijst.insert(index_kees + 1, "George")  # Voeg George toe naast Kees
+print(lijst)
