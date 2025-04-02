c92f1414e36923bc704d21969ca0241280955584
97092925 <97092925@st.deltion.nl>
2025-04-02 15:06:37 +0200

    klaar

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -1,9 +1,11 @@
-# Opdracht 3 input functie
-# Naam student:
-# Groep:
 
-# Hier komt je code...
-
-# Hier start de for-loop
-
-my_list = []
+pizza_lijst = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+pizza_lijst.sort()
+print(pizza_lijst)
+pizza_lijst.append('macaroni')
+print(pizza_lijst)
+pizza_lijst.remove('olivio')
+print(pizza_lijst)
+print(pizza_lijst[:3])
+print([pizza_lijst[len(pizza_lijst) // 2]])
+print(pizza_lijst[-3:])
