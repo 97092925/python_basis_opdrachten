d6460c05bed517fb7244a2bbb5cb52c87d307c3a
97092925 <97092925@st.deltion.nl>
2025-04-02 14:34:17 +0200

    Klaar

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,19 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+lijst = []
+
+# Dictionaries met gegevens van personen
+persoon1 = {"id": 0, "voornaam": "Jan", "achternaam": "Jansen"}
+persoon2 = {"id": 1, "voornaam": "Piet", "achternaam": "Pietersen"}
+persoon3 = {"id": 2, "voornaam": "Anna", "achternaam": "De Vries"}
+persoon4 = {"id": 3, "voornaam": "Lisa", "achternaam": "Bakker"}
+
+# Toevoegen van de dictionaries aan de lijst
+lijst.extend([persoon1, persoon2, persoon3, persoon4])
+
+# Print de volledige naam van de 2e persoon
+print(lijst[0]["voornaam"], lijst[0]["achternaam"])
+print(lijst[1]["voornaam"], lijst[1]["achternaam"])
+print(lijst[2]["voornaam"], lijst[2]["achternaam"])
+print(lijst[3]["voornaam"], lijst[3]["achternaam"])
