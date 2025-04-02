770d4bdf5f5830540611ce329fabddd88ff2c82e
97092925 <97092925@st.deltion.nl>
2025-04-02 14:36:53 +0200

    klaar

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -1,15 +1,16 @@
-# Opdracht 2 lists
-# Naam student:
-# Groep:
-
-
-rivier_info = {
-    "rijn": ["nederland", "duitsland", "Frankrijk"],
-    "maas": ["nederland", "belgië", "duitsland"],
-    "nijl": ["egypte", "soedan", "oeganda"]
+rivier_info = { 
+    "rijn": ["nederland", "duitsland", "frankrijk"], 
+    "maas": ["nederland", "belgië", "duitsland"], 
+    "nijl": ["egypte", "soedan", "oeganda"] 
 }
 
 rivieren = list(rivier_info.keys())
-# rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
-# Hier jouw code.....
+# Print de naam van de 1e rivier en het 2e land waar het doorheen stroomt
+print(f"De rivier {rivieren[0].capitalize()} stroomt onder andere door {rivier_info[rivieren[0]][1].capitalize()}")
+
+# Print de naam van de 2e rivier en het 1e land waar de 1e rivier doorheen stroomt
+print(f"De rivier {rivieren[1].capitalize()} stroomt onder andere door {rivier_info[rivieren[0]][0].capitalize()}")
+
+# Print de naam van de 3e rivier en het 3e land waar de 3e rivier doorheen stroomt
+print(f"De rivier {rivieren[2].capitalize()} stroomt onder andere door {rivier_info[rivieren[2]][2].capitalize()}")
