a84e3114134d78e3aae3d0236a1b351edab023b7
97092925 <97092925@st.deltion.nl>
2025-04-02 15:03:52 +0200

    klaar

--- opdrachten/060_loops/opdr_5/opdr_4.py
+++ opdrachten/060_loops/opdr_5/opdr_4.py
@@ -1,9 +1,2 @@
-# Opdracht 3 input functie
-# Naam student:
-# Groep:
-
-# Hier komt je code...
-
-# Hier start de for-loop
-
-my_list = []
+resultaten = [x * 3 for x in range(1, 21)]
+print(resultaten[:6])
