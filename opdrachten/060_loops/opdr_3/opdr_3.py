9059426c24c23c5f3cbb05c34cd69a1a0d4f50f9
97092925 <97092925@st.deltion.nl>
2025-04-02 15:00:17 +0200

    klaar

--- opdrachten/060_loops/opdr_3/opdr_3.py
+++ opdrachten/060_loops/opdr_3/opdr_3.py
@@ -1,9 +1,6 @@
-# Opdracht 3 input functie
-# Naam student:
-# Groep:
 
-# Hier komt je code...
-
-# Hier start de for-loop
-
-my_list = []
+resultaten = []
+for i in range(3, 82, 3):
+    resultaat = (i ** 2) / 3
+    resultaten.append(resultaat)
+print(resultaten)
