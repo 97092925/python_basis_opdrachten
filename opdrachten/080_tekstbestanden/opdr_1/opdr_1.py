290c6c415cb641a0597c4b9cbecf3b39793c7a5b
97092925 <97092925@st.deltion.nl>
2025-04-02 15:21:16 +0200

    klaar

--- opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
+++ opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
@@ -1,5 +1,10 @@
-# Opdracht 1 while-loops
-# Naam student:
-# Groep:
+vragen = [
+    "Wat vind je van de huidige regering?",
+    "Wat vind je van de Python-lessen tot nu toe?",
+    "Wat vind jij de mooiste stad van Nederland?"
+]
+with open("enquete_resultaten.txt", "w") as bestand:
+    for vraag in vragen:
+        antwoord = input(vraag + " ")
+        bestand.write(f"{vraag}\n{antwoord}\n\n")
 
-# Jouw code komt hier
