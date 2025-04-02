e84ea641683608c49b5b203b0deceee39a2dd504
97092925 <97092925@st.deltion.nl>
2025-04-02 15:02:19 +0200

    klaar

--- opdrachten/060_loops/opdr_4/opdr_4.py
+++ opdrachten/060_loops/opdr_4/opdr_4.py
@@ -1,9 +1,2 @@
-# Opdracht 3 input functie
-# Naam student:
-# Groep:
-
-# Hier komt je code...
-
-# Hier start de for-loop
-
-my_list = []
+resultaten = [4 * x + 7 for x in range(1, 10)]
+print(resultaten)
