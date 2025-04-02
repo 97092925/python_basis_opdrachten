a0f41da8c60586e1176f3ee18b92af3a62e417a1
97092925 <97092925@st.deltion.nl>
2025-04-02 14:42:11 +0200

    klaar

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -1,7 +1,13 @@
-# Opdracht 1 input function
-# Naam student:
-# Groep:
+import math
 
-# Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
+# Vraag de gebruiker om de lengte van de eerste en tweede zijde
+zijde1 = float(input("Geef de lengte van de eerste zijde\n"))
+zijde2 = float(input("Geef de lengte van de tweede zijde\n"))
+
+# Bereken de schuine zijde met de stelling van Pythagoras
+schuine_zijde = math.sqrt(zijde1**2 + zijde2**2)
+
+# Print de lengte van de schuine zijde
+print(f"\nDe lengte van de schuine zijde is: {schuine_zijde}")
 
 
