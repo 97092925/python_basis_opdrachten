f68a6d2b6751d19f966325a40992b51c1fc5f844
97092925 <97092925@st.deltion.nl>
2025-04-02 15:13:23 +0200

    klaar

--- opdrachten/070_condities/opdr_3/opdr_3.py
+++ opdrachten/070_condities/opdr_3/opdr_3.py
@@ -1,12 +1,19 @@
-# Opdracht 3 condities
-# Naam student:
-# Groep:
-
-
-
 
 normale_toegangsprijs = 12.50
-kortings_percentages = {"baby": 100, "kinderen": 50, "volwassenen": 0, "ouderen": 30}
-leeftijd = {"baby": (0, 2), "kinderen": (3, 18), "volwassenen": (19, 64), "ouderen": (65, 150)}
+kortings_percentages = { "baby": 100, "kinderen": 50, "volwassenen": 0, "ouderen": 30 }
+leeftijd = { "baby": (0, 2), "kinderen": (3, 18), "volwassenen": (19, 64), "ouderen": (65, 150) }
 
-# Hier komt je code...
+leeftijd_bezoeker = int(input("Geef uw leeftijd in aantal jaar: "))
+
+groep = ""
+for categorie, (min_leeftijd, max_leeftijd) in leeftijd.items():
+    if min_leeftijd <= leeftijd_bezoeker <= max_leeftijd:
+        groep = categorie
+        break
+
+korting = kortings_percentages[groep]
+te_betalen = normale_toegangsprijs * (1 - korting / 100)
+
+print(f"U behoort tot de groep {groep}")
+print(f"U krijgt {korting}% korting")
+print(f"U betaalt daarom {te_betalen:.2f}")
