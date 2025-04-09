b0eede6326c7a1cca54d497c8a75776d7ae5741c
97092925 <97092925@st.deltion.nl>
2025-04-09 12:11:41 +0200

    klaar

--- opdrachten/090_functies/opdr_3/opdr_3.py
+++ opdrachten/090_functies/opdr_3/opdr_3.py
@@ -1,20 +1,18 @@
-# Opdracht 1 functies
-# Naam student:
-# Groep:
+import math
 
-
-def kubus_vol(m):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+def kubus_vol(a):
+    volume = a ** 3  
+    return volume
 
 def bol_vol(r):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    volume = (4/3) * math.pi * (r ** 3)  
+    return volume
 
-zijde = 5
-radius = 4
+kubus_zijde = 5
+bol_straal = 4
 
-print(kubus_vol(5))
-print(bol_vol(4))
+kubus_volume = kubus_vol(kubus_zijde)
+bol_volume = bol_vol(bol_straal)
+
+print(f"De inhoud van deze kubus is: {kubus_volume}")
+print(f"De inhoud van deze bol is: {bol_volume}")
