02cf0e49910ca37e64856e71a805de2715073be8
97092925 <97092925@st.deltion.nl>
2025-04-09 12:00:11 +0200

    klaar

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -1,6 +1,28 @@
-# Opdracht 3 Tekst opslaan
-# Naam student:
-# Groep:
+def encrypten(tekst):
+    encrypted_text = ""
+
+    for char in tekst:
+        if char.isalpha():  # Controleer of het een letter is
+            # Bepaal of de letter een kleine letter is
+            start = 97 if char.islower() else 65
+            # Verschuif de letter 5 posities verder in het alfabet
+            encrypted_char = chr((ord(char) - start + 5) % 26 + start)
+            encrypted_text += encrypted_char
+        else:
+            # Voeg niet-alfabetische tekens ongewijzigd toe
+            encrypted_text += char
+
+    return encrypted_text
+
+# Vraag de gebruiker om een tekst in te voeren
+tekst = input("Geef de tekst die je wilt encrypten: ")
+
+# Encrypt de tekst
+encrypted = encrypten(tekst)
+
+# Toon de versleutelde tekst
+print("Encrypted tekst:")
+print(encrypted)
 
 
 
